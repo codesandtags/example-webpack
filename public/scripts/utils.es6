70c60af83cd44e0d69391406d01942dde572c8f8
Edwin Torres <codesandtags@gmail.com>
2017-01-21 17:15:38 -0500

    feat: Added autoprefixer and animation to CSS warrior image 🤺

--- public/scripts/utils.es6
+++ public/scripts/utils.es6
@@ -1,1 +1,17 @@
 console.log('This is the utils file');
+
+const esparta = document.querySelector('.warrior');
+const addAnimation = (event) => {
+    event.target.style.animationDuration = '1s';
+    event.target.style.animationName = 'changewidth';
+    event.target.style.animationIterationCount = 5;
+    event.target.style.animationDirection = 'alternate';
+    console.log('added style!');
+};
+
+const removeAnimation = (event) => {
+    event.target.style.animationName = '';
+};
+
+esparta.addEventListener('click', addAnimation);
+esparta.addEventListener('dblclick', removeAnimation);
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin'); // Plugin to extract content in bundles
 
 module.exports = {
     context: path.resolve('./public/scripts'),
     entry: ["./main.js", "./utils.es6"],
 
     output: {
-        publicPath: '/scripts/', /* This is the virtual path */
-        path: path.resolve('./public/scripts/'), /* This is the real output path */
+        path: path.resolve('./public/assets/'), /* This is the real output path */
+        publicPath: '/assets/', /* This is the virtual path */
         filename: "bundle.js"
     },
+
+    plugins: [
+        new ExtractTextPlugin('styles.css')
+    ],
 
     devServer: {
         contentBase: 'public'
@@ -31,12 +36,12 @@ module.exports = {
             {
                 test: /\.css$/,
                 exclude: /node_modules/,
-                loader: "style-loader!css-loader"
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
             },
             {
                 test: /\.scss$/,
                 exclude: /node_modules/,
-                loader: "style-loader!css-loader!sass-loader"
+                loader: ExtractTextPlugin.extract('style-loader', '!css-loader!sass-loader')
             }
         ]
     },
